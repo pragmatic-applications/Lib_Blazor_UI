f67dfaa646b1974be881e522d06cbaae3f1d37c7
Maurice Kelly <kay86@live.com>
2021-08-11 15:27:44 +0100

    Lib_Blazor_UI NuGet <Version>0.0.0.2</Version>

---
+++ docs/Record.cs
@@ -0,0 +1,31 @@
+//=================================================================
+// Solution: Lib_Blazor_UI
+//=================================================================
+// Programmer: Maurice Antonio Kelly @MAKMaurice
+// Description: A To Do App with undo, redo and database CRUD features.
+// ASP.NET Core 5.0
+// Date: 2021-08-11
+// Stop: 16:00
+//=================================================================
+
+// Lib_Blazor_UI NuGet <Version>0.0.0.2</Version>
+
+/*
+
+NuGet Info:
+<PropertyGroup>
+    <TargetFramework>net5.0</TargetFramework>
+    <GeneratePackageOnBuild>true</GeneratePackageOnBuild>
+    <Version>0.0.0.2</Version>
+    <RepositoryUrl>https://github.com/pragmatic-applications/Lib_Blazor_UI</RepositoryUrl>
+    <Authors>Maurice Antonio Kelly @MAKMaurice</Authors>
+    <Company>Pragmatic Applications</Company>
+    <PackageLicenseExpression>MIT Licence</PackageLicenseExpression>
+    <Description>A .NET 5.0 (Blazor) Razor Class Library</Description>
+    <PackageReleaseNotes>A .NET 5.0 (Blazor) Razor Class Library</PackageReleaseNotes>
+    <PackageTags>.NET 5.0 (Blazor) Razor Class Library</PackageTags>
+    <RepositoryType>A .NET 5.0 (Blazor) Razor Class Library</RepositoryType>
+    <Copyright>Pragmatic Applications - Maurice Antonio Kelly @MAKMaurice</Copyright>
+</PropertyGroup>
+
+//*/
