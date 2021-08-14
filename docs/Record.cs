f9f7f3a974f534dd0008113823918783068dc2df
Maurice Kelly <kay86@live.com>
2021-08-14 12:29:16 +0100

    Package update

--- docs/Record.cs
+++ docs/Record.cs
@@ -4,28 +4,14 @@
 // Programmer: Maurice Antonio Kelly @MAKMaurice
 // Description: A To Do App with undo, redo and database CRUD features.
 // ASP.NET Core 5.0
-// Date: 2021-08-11
-// Stop: 16:00
+// Date: 2021-08-14
+// Stop: 14:00
 //=================================================================
 
 // Update NuGet licence Key Expression form MIT licence to MIT, it seems that spaces are not allowed.
 
 /*
 
-NuGet Info:
-<PropertyGroup>
-    <TargetFramework>net5.0</TargetFramework>
-    <GeneratePackageOnBuild>true</GeneratePackageOnBuild>
-    <Version>0.0.0.2</Version>
-    <RepositoryUrl>https://github.com/pragmatic-applications/Lib_Blazor_UI</RepositoryUrl>
-    <Authors>Maurice Antonio Kelly @MAKMaurice</Authors>
-    <Company>Pragmatic Applications</Company>
-    <PackageLicenseExpression>MIT Licence</PackageLicenseExpression>
-    <Description>A .NET 5.0 (Blazor) Razor Class Library</Description>
-    <PackageReleaseNotes>A .NET 5.0 (Blazor) Razor Class Library</PackageReleaseNotes>
-    <PackageTags>.NET 5.0 (Blazor) Razor Class Library</PackageTags>
-    <RepositoryType>A .NET 5.0 (Blazor) Razor Class Library</RepositoryType>
-    <Copyright>Pragmatic Applications - Maurice Antonio Kelly @MAKMaurice</Copyright>
-</PropertyGroup>
+
 
 //*/
